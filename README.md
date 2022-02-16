7cc7952a29eb6bd203c1155482a02d0fe17c73d6
root <root@DESKTOPJLNSNII.localdomain>
2022-02-16 11:51:55 +0100

    Added the loop on homepage

---
+++ README.md
@@ -0,0 +1,3 @@
+#Projet de découverte de wordpress
+
+Ce repository représente le projet de test à l'introduction de wordpress à la HEPL
--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,5 +1,36 @@
 <?php get_header();?>
-<!-- Le contenu de mon template -->
+<main class="layout">
+    <section class="layout__latest latest">
+        <h2 class="latest__title">Mes derniers articles</h2>
+        <div class="letest__container">
+            <?php if(have_posts()): while(have_posts()): the_post(); ?>
+            <!--On est dans sle boucle-->
+                <article class="post">
+                    <a href="<?=get_the_permalink()?>" class="post__link">Lire l'article "<?=get_the_title()?>"</a>
+                    <div class="post__card">
+                        <header class="post__head">
+                            <h3 class="post__title">
+                                <?= get_the_title()?>
+                            </h3>
+                                <p class="post__meta">Publier par <?=get_the_author()?> le <time class="post__date" datetime="<?=get_the_date('c')?>"><?=get_the_date()?></time></p>
+                        </header>
+                                <figure class="post__fig">
+                                    <?= get_the_post_thumbnail(null,'medium_large',['class'=>'post__thumb', 'id'=>'test']);?>
+<!--                                    <img src="" alt="TODO" class="post__thumb">-->
+                                </figure>
+                            <div class="post__excerpt">
+                                <p>
+                                   <?= get_the_excerpt()?>
+                                </p>
+                            </div>
+                    </div>
+                </article>
+            <?php endwhile;else:?>
+            <!--Il n'y a pas d'articles à afficher-->
+            <?php endif;?>
+        </div>
+    </section>
+</main>
 <?php  get_footer()?>
 
 <?php//BEM = Base__Element--Modifier.
