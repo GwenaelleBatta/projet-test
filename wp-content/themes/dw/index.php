cca2c328aca6bc3450124182668ac8fcc0b242e1
Gwen <gwenaelle.batta@studetn.hepl.be>
2022-02-23 15:05:53 +0100

    Added ACF field for trip post_type

--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -41,7 +41,7 @@
 				<div class="trip__card">
 					<header class="trip__head">
 						<h3 class="trip__title"><?= get_the_title()?></h3>
-						<p class="trip__date"> Publier par <?=get_the_author()?> le <time class="trip__time" datetime="">XX/XX/XXXX</time></p>
+						<p class="trip__date"> Publier par <?=get_the_author()?> <time class="trip__time" datetime="<?= date_i18n('c', strtotime(get_field('departure_date',false,false)))?>"><?= ucwords(date_i18n('F, Y', strtotime(get_field('departure_date',false,false))))?></time></p>
 					</header>
 					<figure class="trip__fig">
 						<?= get_the_post_thumbnail(null,'medium',['class'=>'trip__thumb']);?>
